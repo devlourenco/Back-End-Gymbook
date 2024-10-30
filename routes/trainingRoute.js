3a02b53b4773d9bcaafd4e887f174b2bd204ed26
devlourenco <guilherme.gsldev@gmail.com>
2024-10-30 15:22:04 -0300

    Criação de todos os métodos HTTP para a rota de treino (ainda sem testes[)

--- routes/trainingRoute.js
+++ routes/trainingRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const trainingModel = require("../model/trainingModel");
+const { where } = require("sequelize");
 
 const router = express.Router();
 
@@ -24,6 +25,60 @@ router.post("/training", (req, res) => {
         errorStatus: true,
         mensageStatus: "Erro ao registrar treino",
         errorObject: error,
+      });
+    });
+});
+
+router.get("listTraining/:idTreino", (req, res) => {
+  let { idTreino } = req.params;
+
+  trainingModel
+    .findByPK(idTreino)
+    .then((training) => {
+      if (!training) {
+        return res.status(404).json({
+          errorStatus: true,
+          mensageStatus: "Treino não encontrado",
+        });
+      }
+      return res.status(200).json({
+        errorStatus: false,
+        mensageStatus: "Lista de Treinos",
+        training,
+      });
+    })
+    .catch((error) => {
+      return (401).json({
+        errorStatus: true,
+        mensageStatus: "Erro ao listar os treinos",
+        errorObject: error,
+      });
+    });
+});
+
+router.delete("/deleteTraining/:idTreino", (req, res) => {
+  let { idTreino } = req.params;
+
+  trainingModel
+    .destroy({ where: { idTreino } })
+    .then((deleted) => {
+      if (!deleted) {
+        return res.status(404).json({
+          errorStatus: true,
+          mensageStatus: "Treino não encontrado",
+          errorObject: error,
+        });
+      }
+      return res.status(201).json({
+        errorStatus: false,
+        mensageStatus: "Treino Deletado com sucesso!",
+      });
+    })
+    .catch((error) => {
+      return res.status(401).json({
+        errorStatus: true,
+        mensageStatus: "Erro ao deletar o treino",
+        errorObject: false,
       });
     });
 });
